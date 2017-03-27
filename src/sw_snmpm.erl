2e49c1f375dcf33103a76d781007506b808839d3
fxmy <fxmywc@gmail.com>
2017-03-27 20:58:47 +0800

    gen_server in sw_snmp

--- src/sw_snmpm.erl
+++ src/sw_snmpm.erl
@@ -1,8 +1,52 @@
 -module(sw_snmpm).
+-behavior(gen_server).
 
 -include_lib("snmp/include/snmp_types.hrl").
 
+-export([start_link/3]).
+-export([init/1,
+         handle_info/2,
+         terminate/2]).
 -export([table/3]).
+
+-record(state,{snmpm_user,snmpm_agent,
+               scalars=[],tables=[]}).
+
+
+-spec start_link(User::term(),Agent::term(),proplists:prolist()) ->
+  {ok, pid()} | {error, term()}.
+start_link(User, Agent, ManagedObjects) ->
+  gen_server:start_link(?MODULE, [User, Agent, ManagedObjects], []).
+
+
+-spec init([any(),...]) ->
+  {ok, #state{}} |
+  {ok, #state{}, Timeout :: non_neg_integer()} |
+  ignore |
+  {stop, Reason :: any()}.
+init([User, Agent, ManagedObjects]) ->
+  Scalars = proplists:get_all_values(scalar, ManagedObjects),
+  Tables = proplists:get_all_values(table, ManagedObjects),
+  ok = syn:join(sw_topo, self()),
+  {ok, #state{snmpm_user = User,
+              snmpm_agent = Agent,
+              scalars = Scalars,
+              tables = Tables}}.
+
+
+-spec handle_info(Info :: atom(), #state{}) ->
+  {noreply, #state{}} |
+  {noreply, #state{}, Timeout :: non_neg_integer()} |
+  {stop, Reason :: any(), #state{}}.
+handle_info(update_lldp, State) ->
+  %% TODO
+  {noreply, State}.
+
+
+-spec terminate(Reason :: any(), #state{}) -> terminated.
+terminate(Reason, _State) ->
+  error_logger:info_msg("Terminating sw_snmpm ~p with reason: ~p", [#state.snmpm_agent, Reason]),
+  terminated.
 
 
 -spec table(User, Agent, Name) ->
@@ -52,11 +96,17 @@ table_recurse(User, Agent, OidRoot, OidPre, Acc) ->
 %  end.
 
 
+-spec extract_oid({noError, _ErrIdx :: snmpm:error_index(), [#varbind{}]}) ->
+  Oid :: snmpm:oid().
 extract_oid({noError, _ErrIdx, [Varbind=#varbind{}]}) ->
   Varbind#varbind.oid;
 extract_oid(_) ->
   [].
 
+
+-spec update_acc({noError, _ErrIdx :: snmpm:error_index(), [#varbind{}]},
+                 Acc :: proplists:proplist()) ->
+  {Acc1 :: proplists:prolist(), Oid :: snmpm:oid()}.
 update_acc({noError, _ErrIdx, [Varbind=#varbind{}]}, Acc) ->
   Oid = Varbind#varbind.oid,
   VarType = Varbind#varbind.variabletype,
@@ -74,9 +124,13 @@ update_acc({noError, _ErrIdx, [Varbind=#varbind{}]}, Acc) ->
   {Acc1, Oid}.
 
 
+-spec mib_entry_type(Oid :: snmpm:oid()) ->
+  {[integer()], atom()}.
 mib_entry_type(Oid) when is_list(Oid) ->
   mib_entry_type(Oid, []).
 
+-spec mib_entry_type(Oid :: snmpm:oid(), [integer()]) ->
+  {[integer()], atom()}.
 mib_entry_type(Oid, EntryIdx) ->
   %error_logger:info_msg("mib_entry_type: ~p, ~p", [Oid, EntryIdx]),
   case snmpm:oid_to_name(Oid) of
