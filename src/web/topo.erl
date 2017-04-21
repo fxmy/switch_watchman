e06079c47a33895cd2b6bb601d2e9aa2514edb6b
fxmy <fxmywc@gmail.com>
2017-04-21 17:36:32 +0800

    proof of concept of web events

--- src/web/topo.erl
+++ src/web/topo.erl
@@ -5,7 +5,7 @@
 -include_lib("n2o/include/wf.hrl").
 
 main() ->
-  wf:info(?MODULE, "~p~n", [wf:path(?REQ)]),
+  % wf:info(?MODULE, "~p, ~p~n", [wf:session_id(), self()]),
   #dtl{file="topo",
        app=switch_watchman,
        bindings=[{body,[<<"和出错"/utf8>>]},
@@ -16,3 +16,21 @@ dev()  -> [ [ #script{src=lists:concat(["/n2o/protocols/",X,".js"])} || X <- [be
             [ #script{src=lists:concat(["/n2o/",Y,".js"])}           || Y <- [bullet,xhr,n2o,ftp,utf8,validation] ],
             [ #script{src="/assets/vis.min.js"}]
           ].
+
+
+event(init) ->
+  Res = wf:async("topo_countdown", fun topo:loop/1),
+  n2o_async:send("topo_countdown", {5000,self()}),
+  wf:info(?MODULE, "init ~p, ~p~n", [self(), Res]);
+event(terminate) ->
+  wf:stop("topo_countdown"),
+  wf:info(?MODULE, "terminate ~p~n", [self()]);
+event(E) ->
+  wf:info(?MODULE, "event ~p, ~p~n", [E, self()]).
+
+
+loop({T,Pid}) when is_integer(T), is_pid(Pid) ->
+  wf:info(?MODULE, "loop target: ~p~n", [Pid]),
+  % adhere N2O client prottocol
+  % erlang:send_after(T, Pid, {client, update_lldp}),
+  timer:apply_after(5000, wf, send, []).
